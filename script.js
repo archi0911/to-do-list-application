73f070a0ed8be1cac4cb5c80e0fd3b32e2c715af
archi0911 <archiprajapati09@gmail.com>
2026-03-05 23:33:55 +0530

    Implemented CRUD operations

--- script.js
+++ script.js
@@ -1,51 +1,90 @@
 function addButton() {
-	document.getElementById("add-task").style.display = "block";
-  document.getElementById("ok").style.display = "block";
+	document.getElementById("add-task").style.display = "inline-block";
+	document.getElementById("ok-btn").style.display = "inline-block";
 }
 
 function updateButton() {
-	document.getElementById("update-task").style.display = "block";
-  document.getElementById("update").style.display = "block";
+	document.getElementById("update-task").style.display = "inline-block"; 
+	document.getElementById("update-btn").style.display = "inline-block"; 
+}
+
+function removeButton() {
+	document.getElementById("remove-task").style.display = "inline-block"; 
+	document.getElementById("remove-btn").style.display = "inline-block"; 
 }
 
 let count = 1;
 
 function generateNo() {
 	return count++;
-
 }
 
-function storeData() {
+function addTask() {
 	let data = document.getElementById("add-task").value;
-
 	let num = generateNo();
+
 	localStorage.setItem(num, data);
 
-	// document.getElementById("list").innerHTML += num + " " + localStorage.getItem(num) + ` <input type="checkbox"><br>`;
 	document.getElementById("list").innerHTML += 
-	`<div class="list-item">
-	<span class="num">${num}</span>
-	<span class="task">${localStorage.getItem(num)}</span>
-	<input type="checkbox">
-	</div>`;
-		document.getElementById("list").appendChild("list");
+		`<div class="list-item">
+		<span class="num">${num}</span>
+		<span class="task">${localStorage.getItem(num)}</span>
+		<input type="checkbox">
+		</div>`;
+	document.getElementById("list").appendChild("list");
 
+	// document.getElementById("list").innerHTML += num + " " + localStorage.getItem(num) + ` <input type="checkbox"><br>`;
 }
 
+function updateTask() {
+	let key = document.getElementById("update-task").value;
+	let newtask = document.getElementById("add-task").value;
 
-function updateData() {
-	let data = document.getElementById("update-task").value;
+	if (localStorage.getItem(key)) {
+		localStorage.setItem(key, newtask);
 
-
-}
-
-function removeData() {
-	// let key = document.getElementById("remove").value;
-
-	for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
-    console.log(`${key}: ${value}`);
+		let taskElements = document.querySelectorAll(".list-item");
+        taskElements.forEach(item => {
+            if (item.querySelector(".num").innerText == key) {
+                item.querySelector(".task").innerText = newtask;
+            }
+        });
+	}
+	else {
+			console.log("");
 	}
 
+}
+
+// function removeTask() {
+// 	// Get the key from the input box
+// 	let key1 = document.getElementById("remove").value;
+
+// 	if (localStorage.getItem(key1)) {
+// 			localStorage.removeItem(key1);
+// 	} 
+// 	else {
+// 			console.log("");
+// 	}
+   
+function removeTask() {
+    let key1 = document.getElementById("remove-task").value;
+
+    if (localStorage.getItem(key1)) {
+        // Remove from localStorage
+        localStorage.removeItem(key1);
+
+        // Remove from page
+        let taskElements = document.querySelectorAll(".list-item");
+        taskElements.forEach(item => {
+            if (item.querySelector(".num").innerText == key1) {
+                item.remove();
+            }
+        });
+
+        console.log("");
+    } 
+		else {
+        console.log("");
+    }
 }
