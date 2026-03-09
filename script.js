06c4512dfe3ed81d564a82c373d0d8ffbcc9de14
archi0911 <archiprajapati09@gmail.com>
2026-03-09 17:53:24 +0530

    Made changes in the CSS and Js files

--- script.js
+++ script.js
@@ -1,90 +1,99 @@
 function addButton() {
-	document.getElementById("add-task").style.display = "inline-block";
+	document.getElementById("task-textbox").style.display = "inline-block";
 	document.getElementById("ok-btn").style.display = "inline-block";
 }
 
-function updateButton() {
-	document.getElementById("update-task").style.display = "inline-block"; 
-	document.getElementById("update-btn").style.display = "inline-block"; 
-}
-
-function removeButton() {
-	document.getElementById("remove-task").style.display = "inline-block"; 
-	document.getElementById("remove-btn").style.display = "inline-block"; 
-}
-
-let count = 1;
-
-function generateNo() {
-	return count++;
-}
-
 function addTask() {
-	let data = document.getElementById("add-task").value;
-	let num = generateNo();
-
+	let textbox = document.getElementById("task-textbox");
+	let data = textbox.value;
+	// let num = Math.floor(Math.random()*100);
+	let num = Date.now();
 	localStorage.setItem(num, data);
 
-	document.getElementById("list").innerHTML += 
-		`<div class="list-item">
-		<span class="num">${num}</span>
-		<span class="task">${localStorage.getItem(num)}</span>
-		<input type="checkbox">
-		</div>`;
-	document.getElementById("list").appendChild("list");
+	viewList();
+	// reset textbox
+	textbox.value = "";
 
-	// document.getElementById("list").innerHTML += num + " " + localStorage.getItem(num) + ` <input type="checkbox"><br>`;
 }
 
-function updateTask() {
-	let key = document.getElementById("update-task").value;
-	let newtask = document.getElementById("add-task").value;
+function viewList() {
+
+	let list = document.getElementById("list");
+	list.innerHTML = "<h1>List:</h1>";
+
+	for (let i = 0; i < localStorage.length; i++) {
+
+		let key = localStorage.key(i);
+		let value = localStorage.getItem(key);
+
+		list.innerHTML += `
+			<div class="list-item">
+				<input type="checkbox">
+				<span class="task">${value}</span>
+				<i class="fa fa-pencil" onclick="updateTask('${key}')"></i>
+		    <i class="material-icons" onclick="removeTask('${key}')">&#xe872;</i>
+			</div>`;
+
+// 		document.getElementById("list").innerHTML += 
+// 			`<h1>List: </h1>
+// 			<div class="list-item">
+// 				<span class="task">${(value)}</span>
+// 				<input type="checkbox">
+// 			</div>`;
+// 		document.getElementById("list").appendChild("list");
+
+	// document.getElementById("list").innerHTML += num + " " + localStorage.getItem(num) + ` <input type="checkbox"><br>`;
+
+	}
+}
+
+function updateTask(key) {
+	let newtask = document.getElementById("task-textbox").value;
 
 	if (localStorage.getItem(key)) {
 		localStorage.setItem(key, newtask);
-
-		let taskElements = document.querySelectorAll(".list-item");
-        taskElements.forEach(item => {
-            if (item.querySelector(".num").innerText == key) {
-                item.querySelector(".task").innerText = newtask;
-            }
-        });
+		viewList(); // refresh the list
 	}
-	else {
-			console.log("");
-	}
-
 }
 
-// function removeTask() {
-// 	// Get the key from the input box
-// 	let key1 = document.getElementById("remove").value;
+function updateTask(key) {
 
-// 	if (localStorage.getItem(key1)) {
-// 			localStorage.removeItem(key1);
-// 	} 
-// 	else {
-// 			console.log("");
-// 	}
-   
-function removeTask() {
-    let key1 = document.getElementById("remove-task").value;
+	let textbox = document.getElementById("task-textbox");
+	let okBtn = document.getElementById("ok-btn");
 
-    if (localStorage.getItem(key1)) {
-        // Remove from localStorage
-        localStorage.removeItem(key1);
+	// show input field
+	textbox.style.display = "inline-block";
+	okBtn.style.display = "inline-block";
 
-        // Remove from page
-        let taskElements = document.querySelectorAll(".list-item");
-        taskElements.forEach(item => {
-            if (item.querySelector(".num").innerText == key1) {
-                item.remove();
-            }
-        });
+	// change button text
+	okBtn.innerText = "Update";
 
-        console.log("");
-    } 
-		else {
-        console.log("");
-    }
+	// fill textbox with existing task
+	// textbox.value = localStorage.getItem(key);
+
+	// change OK button action to update
+	okBtn.onclick = function() {
+
+		let newtask = textbox.value;
+
+		if(newtask.trim() === "") return;
+
+		localStorage.setItem(key, newtask);
+
+		viewList();
+
+		// reset textbox
+		textbox.value = "";
+
+		// restore button text
+		okBtn.innerText = "OK";
+
+		// restore add mode
+		okBtn.onclick = addTask;
+	};
+}
+
+function removeTask(key){
+	localStorage.removeItem(key);
+	viewList();
 }
