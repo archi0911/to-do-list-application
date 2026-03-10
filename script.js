7af158642d7767e2f28eae40ca44a4d204bfca2a
archi0911 <archiprajapati09@gmail.com>
2026-03-10 11:02:14 +0530

    Added the completedList() and added a <div> tag

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function addTask() {
 	let data = textbox.value;
 	// let num = Math.floor(Math.random()*100);
 	let num = Date.now();
+
+	if (data === "") return; 
+
 	localStorage.setItem(num, data);
 
 	viewList();
@@ -19,42 +22,55 @@ function addTask() {
 function viewList() {
 
 	let list = document.getElementById("list");
-	list.innerHTML = "<h1>List:</h1>";
+	list.innerHTML = "<h1>Pending Task:</h1>";
 
 	for (let i = 0; i < localStorage.length; i++) {
 
 		let key = localStorage.key(i);
+
+		if (key.startsWith("completed-")) {
+			continue; // skip completed tasks
+		}
+
 		let value = localStorage.getItem(key);
 
 		list.innerHTML += `
 			<div class="list-item">
-				<input type="checkbox">
+			
+				<input type="checkbox" onchange="completeTask('${key}')">
 				<span class="task">${value}</span>
 				<i class="fa fa-pencil" onclick="updateTask('${key}')"></i>
 		    <i class="material-icons" onclick="removeTask('${key}')">&#xe872;</i>
 			</div>`;
-
-// 		document.getElementById("list").innerHTML += 
-// 			`<h1>List: </h1>
-// 			<div class="list-item">
-// 				<span class="task">${(value)}</span>
-// 				<input type="checkbox">
-// 			</div>`;
-// 		document.getElementById("list").appendChild("list");
-
+			
 	// document.getElementById("list").innerHTML += num + " " + localStorage.getItem(num) + ` <input type="checkbox"><br>`;
-
 	}
 }
 
-function updateTask(key) {
-	let newtask = document.getElementById("task-textbox").value;
+function completeTask(key){
+	let task = localStorage.getItem(key);
 
-	if (localStorage.getItem(key)) {
-		localStorage.setItem(key, newtask);
-		viewList(); // refresh the list
+	let completed = document.getElementById("completed-list");
+
+	if (completed.innerHTML === "") {
+		completed.innerHTML = "<h1>Completed Task:</h1>";
 	}
+
+	completed.innerHTML += `
+	<div class="list-item">
+		<input type="checkbox" checked>
+		<span class="task">${task}</span>
+	</div>`;
+
+	// store completed task
+	localStorage.setItem("completed-" + key, task);
+
+	// remove from active list
+	localStorage.removeItem(key);
+
+	viewList();
 }
+
 
 function updateTask(key) {
 
@@ -65,11 +81,10 @@ function updateTask(key) {
 	textbox.style.display = "inline-block";
 	okBtn.style.display = "inline-block";
 
-	// change button text
 	okBtn.innerText = "Update";
 
 	// fill textbox with existing task
-	// textbox.value = localStorage.getItem(key);
+	textbox.value = localStorage.getItem(key);
 
 	// change OK button action to update
 	okBtn.onclick = function() {
@@ -84,10 +99,8 @@ function updateTask(key) {
 
 		// reset textbox
 		textbox.value = "";
-
 		// restore button text
 		okBtn.innerText = "OK";
-
 		// restore add mode
 		okBtn.onclick = addTask;
 	};
